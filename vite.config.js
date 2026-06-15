e23f23960e41ab7cbbde466edb724fb0c2436155
SubDSR <davidsevanr@gmail.com>
2026-06-14 20:55:16 -0500

    chore: release 2.3.1

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // El avatar 3D carga librerias pesadas en un chunk lazy separado.
+    chunkSizeWarningLimit: 1200,
+  },
 });
